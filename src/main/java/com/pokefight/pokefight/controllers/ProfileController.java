e5e65f677c40c64bba5a221fc887111ea1299433
Noah Bruno <noah.bruno122@gmail.com>
2022-05-03 09:13:05 -0500

    s

--- src/main/java/com/pokefight/pokefight/controllers/ProfileController.java
+++ src/main/java/com/pokefight/pokefight/controllers/ProfileController.java
@@ -1,17 +1,45 @@
 package com.pokefight.pokefight.controllers;
 
+import com.pokefight.pokefight.models.User;
+import com.pokefight.pokefight.repositories.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.*;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class ProfileController {
-    @GetMapping("/profile")
-    public String profileGet(){
-        return "temporary/profile";
+
+    private UserRepository userDao;
+
+    public ProfileController(UserRepository userDao){
+        this.userDao=userDao;
     }
 
-    @PostMapping("/profile")
-    public String profilePost(){
-        return "temporary/profile";
+//    @GetMapping("/profile")
+//    public String profileGet(){
+//        return "temporary/profile";
+//    }
+//
+//    @PostMapping("/profile")
+//    public String profilePost(){ return "temporary/profile"; }
+
+    @GetMapping("/profile/{username}")
+    public String gUsers(@PathVariable String username ,Model model) {
+        User user = userDao.findByUsername(username);
+        model.addAttribute("username",user);
+        return"temporary/profile";
+}
+    @GetMapping("/profile/edit")
+    public String editProfile( Model model) {
+        model.addAttribute("user", new User());
+        return"temporary/profile";
     }
+
+    @PostMapping("/profile/edit")
+    public String submitEdit(@ModelAttribute User user) {
+        userDao.save(user);
+        return "redirect:temporary/profile";
+    }
+
 }
