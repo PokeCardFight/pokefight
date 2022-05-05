38bd49d599b24fd9753d131f9262e1fe0685e37e
Gerald Montero <geraldmontero12@gmail.com>
2022-05-05 16:48:15 -0500

    updated home controller

--- src/main/java/com/pokefight/pokefight/controllers/HomeController.java
+++ src/main/java/com/pokefight/pokefight/controllers/HomeController.java
@@ -3,14 +3,16 @@ package com.pokefight.pokefight.controllers;
 import com.pokefight.pokefight.models.User;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.*;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HomeController {
     @GetMapping("/home")
-    public String homeGet(){
+    public String homeGet(Model model){
 
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        model.addAttribute("user", user);
         return "/home";
     }
 
