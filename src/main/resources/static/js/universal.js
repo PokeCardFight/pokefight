7cdef085b13c3d7218220ff9ae87760563be0831
Manuel Dominguez <manuel.dominguez1113@gmail.com>
2022-05-06 18:55:01 -0500

    completed navbar. moved login to navbar.added password toggle visibility on form. empty splash page. i will add fluff to the splash over weekend

---
+++ src/main/resources/static/js/universal.js
@@ -0,0 +1,10 @@
+$("#logIn").click(() => {
+    $("#logInForm").toggleClass("hidden active");
+})
+const password =$("#loginPassword")
+const passwordToggle = $("#loginPasswordToggle")
+passwordToggle.click(()=>{
+    const type = password.attr("type") === "password"? "text": "password";
+    password.attr("type", type);
+    passwordToggle.children().toggleClass("fa-eye fa-eye-slash")
+})
