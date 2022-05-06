6358a342bc712861e75d9359c43060e3f66fe3d5
Michael Jones <michael.k.jones.20@gmail.com>
2022-05-06 09:51:34 -0500

    Update BattleController.java

--- src/main/java/com/pokefight/pokefight/controllers/BattleController.java
+++ src/main/java/com/pokefight/pokefight/controllers/BattleController.java
@@ -3,10 +3,18 @@ package com.pokefight.pokefight.controllers;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 @Controller
 public class BattleController {
+
+    private boolean flipCoin() { return ThreadLocalRandom.current().nextBoolean(); }
+
     @GetMapping("/battle")
     public String aboutGet(){
+
+        System.out.println("flipCoin() = " + flipCoin());
+
         return "/battle";
     }
 
