0111fba108dff81169f504153d8e0c0d5b3ca0e9
Michael Jones <michael.k.jones.20@gmail.com>
2022-05-07 00:03:42 -0500

    Updates to battle controller.

--- src/main/java/com/pokefight/pokefight/controllers/BattleController.java
+++ src/main/java/com/pokefight/pokefight/controllers/BattleController.java
@@ -1,6 +1,8 @@
 package com.pokefight.pokefight.controllers;
 
 import com.pokefight.pokefight.models.*;
+import com.pokefight.pokefight.repositories.CardRepository;
+import com.pokefight.pokefight.repositories.UserRepository;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.*;
 import org.springframework.ui.Model;
@@ -11,30 +13,53 @@ import java.util.concurrent.ThreadLocalRandom;
 @Controller
 public class BattleController {
 
-//    User user;
-//    Card playerCard, computerCard;
-//
-//    private BattleController() {
-//        user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//    }
+    private CardRepository cardDao;
+
+    boolean turn;
+    long playerCardId;
+    long playerPouchId;
+    long computerCardId;
 
     private boolean flipCoin() { return ThreadLocalRandom.current().nextBoolean(); }
+    private long getRandomCardId() { return (long)(Math.random() * 150) + 1; }
 
-    @GetMapping("/battle/{cardId}/{pouchId}/")
-    public String aboutGet(Model model, @PathVariable(value = "cardId") long cardId, @PathVariable(value = "pouchId") long pouchId){
+    public BattleController(CardRepository cardDao) {
+        this.cardDao = cardDao;
+    }
 
-        boolean isHeads = flipCoin();
-        String currentTurn;
-        if (isHeads) currentTurn = "Player";
-        else currentTurn = "Computer";
+    @GetMapping("/battle")
+    public String battleView(Model model){
 
-        model.addAttribute("turn", currentTurn);
+        if (turn) model.addAttribute("turn", "Player");
+        else model.addAttribute("turn", "Computer");
+
+        Card playerCard = cardDao.getById(playerCardId);
+//        Card computerCard = cardDao.getById(computerCardId);
+        model.addAttribute("playerCard", playerCard.getName());
+//        model.addAttribute("computerCard", computerCard.getName());
 
         return "/battle";
     }
 
+    @GetMapping("/battle/{cardId}/{pouchId}/")
+    public String battleGet(@PathVariable(value = "cardId") long cardId, @PathVariable(value = "pouchId") long pouchId){
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        turn = flipCoin();
+
+        playerCardId = cardId;
+        playerPouchId = pouchId;
+
+//        int userLevel = user.getLevel();
+//        if(userLevel >= 1 && userLevel <= 5) computerCardId = cardDao.getRandomCommonCard();
+//        if(userLevel >= 6 && userLevel <= 10) computerCardId = cardDao.getRandomUncommonCard();
+//        if(userLevel >= 11 && userLevel <= 15) computerCardId = cardDao.getRandomRareCard();
+
+        return "redirect:/battle";
+    }
+
     @PostMapping("/battle")
-    public String aboutPost(){
+    public String battlePost(){
         return "/battle";
     }
 }
