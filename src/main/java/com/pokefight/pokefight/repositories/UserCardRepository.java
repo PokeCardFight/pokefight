5fe6c169c7eed739c4513098c730b038197cdff9
Gerald Montero <geraldmontero12@gmail.com>
2022-05-09 10:10:32 -0500

    passed card urls to views

--- src/main/java/com/pokefight/pokefight/repositories/UserCardRepository.java
+++ src/main/java/com/pokefight/pokefight/repositories/UserCardRepository.java
@@ -3,4 +3,6 @@ package com.pokefight.pokefight.repositories;
 import com.pokefight.pokefight.models.*;
 import org.springframework.data.jpa.repository.*;
 
-public interface UserCardRepository extends JpaRepository<UserCard, Long> {}
+public interface UserCardRepository extends JpaRepository<UserCard, Long> {
+
+}
