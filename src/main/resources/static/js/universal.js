933323a69b3c80ec34a0f5308dc303a691d357a0
Manuel Dominguez <manuel.dominguez1113@gmail.com>
2022-05-13 00:40:19 -0500

    finally a good navbar for mobile

--- src/main/resources/static/js/universal.js
+++ src/main/resources/static/js/universal.js
@@ -1,21 +1,49 @@
-$("#logIn").click(() => {
+$("#logIn, #logInM").click(() => {
     $("#logInForm").toggleClass("hidden active");
 })
-const password =$("#loginPassword")
+const password = $("#loginPassword")
 const passwordToggle = $("#loginPasswordToggle")
-passwordToggle.click(()=>{
-    const type = password.attr("type") === "password"? "text": "password";
+passwordToggle.click(() => {
+    const type = password.attr("type") === "password" ? "text" : "password";
     password.attr("type", type);
     passwordToggle.children().toggleClass("fa-eye fa-eye-slash")
 })
 
 // pull favicon -- faviconLnk = document.querySelector('link[rel*="icon"]');
 
-//logo hyperlink cause anchor wanted to fight me
-/*$('#logo').click(function() {
-    window.location.href = '/';
-})*/
+//nav bar dropdown
+const navLinksM = $("#navLinksM")
+
+$("#dropbtn").click(() => {
+    /* When the user clicks on the button,
+    toggle between hiding and showing the dropdown content */
+    navLinksM.toggleClass("show drop");
+})
+
+
+// Close the dropdown menu if the user clicks outside it
+$(window).click((event) => {
+    if (!event.target.matches(".dropbtn") && !event.target.matches(".dropDown-content") && !event.target.matches("#MListBackground") && !event.target.matches(".unique-navbar")) {
+        if (navLinksM.hasClass("show")) {
+            navLinksM.removeClass("show");
+            navLinksM.addClass("drop");
+        }
+    }
+})
+//navbar hider on bigger screens
+const nav = $(".unique-navbar");
+let lastScrollY = window.scrollY;
+
+$(window).scroll(() => {
+    if (lastScrollY > window.scrollY) {
+        nav.removeClass("nav--hidden");
+    } else {
+        nav.addClass("nav--hidden");
+    }
+    lastScrollY = window.scrollY;
+})
+
 //logout submission connector
-$("#logout").click(()=>{
+$("#logout, #logoutM").click(() => {
     $("form[name='logoutForm']").submit();
 })
