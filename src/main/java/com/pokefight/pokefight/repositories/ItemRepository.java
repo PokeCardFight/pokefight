8f3d69e84bbba11e50f8c17a0622e8c0118b2aac
Noah Bruno <noah.bruno122@gmail.com>
2022-05-13 17:03:26 -0500

    added pokemon and background images for battle

--- src/main/java/com/pokefight/pokefight/repositories/ItemRepository.java
+++ src/main/java/com/pokefight/pokefight/repositories/ItemRepository.java
@@ -9,7 +9,7 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
     @Query(value="select i.id, cost, i.image, name, i.rounds, value from items i join pouch_item pi on i.id = pi.item_id where pouch_id = ?",nativeQuery = true)
     List<Item> getItemsByPouch(long id);
 
-    @Query(value="select image from items join pouch_item pi on items.id = pi.item_id where pouch_id = ?", nativeQuery = true)
-    List<String> getItemImageByPouchId(long id);
+//    @Query(value="select image from items join pouch_item pi on items.id = pi.item_id where pouch_id = ?", nativeQuery = true)
+//    List<String> getItemImageByPouchId(long id);
 
 }
