dcfcac8e9163d3d2901383b25af5bc688c490eb1
NoahBruno <94698680+NoahBruno@users.noreply.github.com>
2022-05-19 15:07:45 -0500

    osiv

--- src/main/java/com/pokefight/pokefight/controllers/BattleController.java
+++ src/main/java/com/pokefight/pokefight/controllers/BattleController.java
@@ -85,7 +85,7 @@ public class BattleController {
         System.out.println("userLevel = " + userLevel);
         System.out.println("computerCardId = " + computerCardId);
 
-        return "redirect:battle";
+        return "redirect:home/default";
     }
 
     @PostMapping("/battle/remove/item")
